7d102fd6457ff3dfabff30cf820b79da858e4577
SadikhovEmin <eemin.sadykhov@gmail.com>
2022-03-28 19:09:41 +0300

    Initial commit

---
+++ src/main/java/com/example/ECommerceSystemBackend/ECommerceSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ECommerceSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceSystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ECommerceSystemBackend/ECommerceSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ECommerceSystemBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
