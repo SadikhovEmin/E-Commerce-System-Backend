5b46e5dc8c9e85c74c4929b61756adc116132ae4
SadikhovEmin <eemin.sadykhov@gmail.com>
2022-04-07 17:47:10 +0300

    Fixes

--- src/test/java/com/example/ECommerceSystemBackend/ECommerceSystemBackendApplicationTests.java
+++ src/test/java/com/example/ECommerceSystemBackend/ECommerceSystemBackendApplicationTests.java
@@ -1,7 +1,7 @@
 package com.example.ECommerceSystemBackend;
 
-import com.example.ECommerceSystemBackend.Model.Customer;
 import com.example.ECommerceSystemBackend.Repository.CustomerRepository;
+import com.example.ECommerceSystemBackend.model.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
