66b987851e81fc051da8e368266726384bac2a89
Esat Simitçioğlu <esad.simitcioglu@ozu.edu.tr>
2022-04-09 00:42:23 +0300

    Add PlatformManager props

--- src/main/java/com/example/ECommerceSystemBackend/Model/PlatformManager.java
+++ src/main/java/com/example/ECommerceSystemBackend/Model/PlatformManager.java
@@ -1,5 +1,62 @@
 package com.example.ECommerceSystemBackend.Model;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class PlatformManager {
-    // Account for Platform Managers should be created manually from Database1
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    @Column(name = "NAME")
+    private String name;
+    @Column(name = "SURNAME")
+    private String surname;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
+
+    public PlatformManager() {
+    }
+
+    public PlatformManager(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
